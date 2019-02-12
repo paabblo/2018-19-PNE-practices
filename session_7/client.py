24132a55e1c73fcc7371d74be4083e3a2b6f08ef
paabblo <pa.martinez.2018@alumnos.urjc.es>
2019-02-12 16:24:22 +0100

    python

---
+++ session_7/client.py
@@ -0,0 +1,14 @@
+# We are programming our first client
+
+
+
+import socket
+
+# Create a socket for communicating with the server
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+print('socket created')
+
+
+

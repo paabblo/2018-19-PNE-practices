9d640e41488fb5b042d3b6e568914ecea6d06cff
paabblo <pa.martinez.2018@alumnos.urjc.es>
2019-03-12 15:57:18 +0100

    python

---
+++ session_15/form-server1.py
@@ -0,0 +1,42 @@
+import http.server
+import socketserver
+import termcolor
+
+
+PORT = 8000
+
+
+class TestHandler(http.server.BaseHTTPRequestHandler):
+
+    def do_GET(self):
+
+        # .. Printing the request line:
+        termcolor.cprint(self.requestline, 'green')
+
+        f = open("form2.html", 'r')
+        contents = f.read()
+
+        self.send_response(200)
+
+        self.send_header('Content-Type', 'text/html')
+        self.send_header('Content-Length', len(str.encode(contents)))
+        self.end_headers()
+
+
+        #... Sending the body of the response msg
+
+        self.wfile.write(str.encode(contents))
+
+
+#.. Main program
+
+with socketserver.TCPServer(("", PORT), TestHandler) as httpd:
+    print("Serving at PORT: {}".format(PORT))
+
+    try:
+        httpd.serve_forever()
+
+    except KeyboardInterrupt:
+        httpd.server_close()
+
+print('The server has stopped')
